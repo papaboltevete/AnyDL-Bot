249f3761666d7983af64bfb8b2c59f8e5e57d8c6
papaboltevete <86361062+papaboltevete@users.noreply.github.com>
2021-06-23 18:58:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1802997328:AAHE-LLyvliJo-X9yY0w40yCTn0BUx8bn1g")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", "5799454")
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "0116b6e4220e6d4c22f86ed7cf26d203")
 
     AUDIO_THUMBNAIL = os.environ.get("AUDIO_THUMBNAIL", "")
 
